2829b51ea50138089e066197d539ff0cc08b6774
Philip Wilcox <philip@philipwilcox.com>
2017-12-25 06:51:38 -0500

    Replace the default component with a routing to a modified version

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloPhil from '@/components/HelloPhil'
+// TODO: How do I get this stuff on the right side of "from" to autocomplete?
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HelloPhil',
+      component: HelloPhil
     }
   ]
 })
